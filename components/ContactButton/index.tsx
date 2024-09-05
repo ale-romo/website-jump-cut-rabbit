dd394e22a360f9cfe66dbb6198a355ec94368a21
alex-romo <alejandro@mintitmedia.com>
2024-09-05 11:32:35 -0700

    feat: updated email

--- components/ContactButton/index.tsx
+++ components/ContactButton/index.tsx
@@ -11,7 +11,7 @@ const ContactButton = ({ title, accent = true }: { title: string, accent?: boole
   return <Popover>
     <PopoverTrigger className={`bg-background/30 opacity-80 hover:opacity-100 font-heading md:font-semibold md:text-xl uppercase border ${accent ? 'border-accent text-accent' : 'border-foreground text-foreground'} px-4 py-2 rounded-lg`}>{title}</PopoverTrigger>
     <PopoverContent className="p-2 flex-row flex items-center gap-3 rounded-lg bg-popover/80" side={side} sideOffset={8}>
-      <a href="mailto:jumpcutrabbit@gmail.com" target="_blank" title="Email"><Image src="/icons/mail-icon.svg" width="0" height="0" className="w-8 h-auto p-1 hover:bg-popover/50 rounded-lg" alt="Email" /></a>
+      <a href="mailto:hello@jumpcutrabbit.com" target="_blank" title="Email"><Image src="/icons/mail-icon.svg" width="0" height="0" className="w-8 h-auto p-1 hover:bg-popover/50 rounded-lg" alt="Email" /></a>
       <a href="https://wa.me/16198976130" target="_blank" title="Whatsapp"><Image src="/icons/whatsapp-icon.svg" width="0" height="0" className="w-8 h-auto p-1 hover:bg-popover/50 rounded-lg" alt="Whatsapp" /></a>
     </PopoverContent>
   </Popover>
